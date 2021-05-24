9670a5e77cf927e9ecb6c1acbd8b8853eacf2880
Jaynil1611 <48921037+Jaynil1611@users.noreply.github.com>
2021-05-24 16:38:27 +0530

    refactor: added NotFound component in index file

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Navbar } from "./Navbar/Navbar";
 export { default as Home } from "./Home/Home";
 export { default as QuizList } from "./QuizList/QuizList";
 export { default as Quiz } from "./Quiz/Quiz";
+export { default as NotFound } from "./NotFound/NotFound";
