21d8c6ec75f264960e843f301b526fe9c223e3e9
Jaynil1611 <48921037+Jaynil1611@users.noreply.github.com>
2021-05-28 16:10:29 +0530

    feat: added axios custom hook & toast component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router";
 import { Home, Navbar, QuizList, Quiz, NotFound } from "./components";
+import { useAxios } from "./server";
 import "./styles.css";
 import { ScrollToTop } from "./utils";
 
 function App() {
+  useAxios("quizzes", "quizzes");
   return (
     <div>
       <Navbar />
--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -3,3 +3,5 @@ export { default as Home } from "./Home/Home";
 export { default as QuizList } from "./QuizList/QuizList";
 export { default as Quiz } from "./Quiz/Quiz";
 export { default as NotFound } from "./NotFound/NotFound";
+export { showToast } from "./Toast/Toast";
+export { default as QuizReview } from "./QuizReview/QuizReview";
