bb8c671ee8db047632e9f8f61f5ebb05e5970ff8
Jaynil1611 <48921037+Jaynil1611@users.noreply.github.com>
2021-05-31 16:33:24 +0530

    refactor: shifted axios header from context to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,24 @@
 import { Routes, Route } from "react-router";
-import { Home, Navbar, QuizList, Quiz, NotFound } from "./components";
+import {
+  Home,
+  Navbar,
+  QuizList,
+  Quiz,
+  NotFound,
+  PrivateRoute,
+  Login,
+  SignUp,
+} from "./components";
+import { useQuiz } from "./contexts";
 import { useAxios } from "./server";
 import "./styles.css";
-import { ScrollToTop } from "./utils";
+import { ScrollToTop, setupAuthHeaderForServerCalls } from "./utils";
 
 function App() {
+  const { token } = useQuiz();
+  setupAuthHeaderForServerCalls(
+    JSON.parse(localStorage.getItem("isUserLoggedIn")!)
+  );
   useAxios("quizzes", "quizzes");
   return (
     <div>
@@ -13,7 +27,13 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/quizzes" element={<QuizList />} />
-        <Route path="/quizzes/:quizId" element={<Quiz />} />
+        <PrivateRoute
+          token={token}
+          path="/quizzes/:quizId"
+          element={<Quiz />}
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
